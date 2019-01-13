0438bf755a80584627171e9b31fc387c50887e4c
davidangulocorcuera <davidangulocorcuera@gmail.com>
2019-01-13 16:02:46 +0100

    firts commit!

---
+++ lib/SecondScreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/ThirdScreen.dart';
+
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Second Screen"),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, position) {
+            return ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text("Element $position"),
+              subtitle: Text("Subtitle $position"),
+              onTap: () async{
+               final int selected = await Navigator.of(context).push(MaterialPageRoute(builder: (c) {
+                  return ThirdScreen(position);
+                }));
+              },
+            );
+          },
+          itemCount: 20,
+          reverse: true,
+        )
+        /*ListTile(title: Text("Element"),leading:  Icon(Icons.account_circle), subtitle: Text("Subtitulo"),),
+          ListTile(title: Text("Element"),leading:  Icon(Icons.account_circle), subtitle: Text("Subtitulo"),),
+          ListTile(title: Text("Element"),leading:  Icon(Icons.account_circle), subtitle: Text("Subtitulo"),),
+          ListTile(title: Text("Element"),leading:  Icon(Icons.account_circle), subtitle: Text("Subtitulo"),),
+          ListTile(title: Text("Element"),leading:  Icon(Icons.account_circle), subtitle: Text("Subtitulo"),),
+          ListTile(title: Text("Element"),leading:  Icon(Icons.account_circle), subtitle: Text("Subtitulo"),),
+          ListTile(title: Text("Element"),leading:  Icon(Icons.account_circle), subtitle: Text("Subtitulo"),),
+          ListTile(title: Text("Element"),leading:  Icon(Icons.account_circle), subtitle: Text("Subtitulo"),),
+          ListTile(title: Text("Element"),leading:  Icon(Icons.account_circle), subtitle: Text("Subtitulo"),),*/
+
+        );
+  }
+}
---
+++ lib/ThirdScreen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/SecondScreen.dart';
+
+class ThirdScreen extends StatefulWidget {
+  final int position;
+
+  @override
+  _ThirdScreenState createState() => _ThirdScreenState();
+
+  ThirdScreen(this.position);
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('elemento ${widget.position}'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(20, (index) {
+          return Center(
+            child: Container(
+              color: Colors.green,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context, index);
+                },
+                child: Text(
+                  'celda $index',
+                  style: Theme.of(context).textTheme.headline,
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
